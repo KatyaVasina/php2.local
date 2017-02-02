6f368fad0800b21121355611fd0d9ceb59485993
KatyaVasina <ddjcnjrr0@mail.ru>
2017-02-02 12:50:58 +0300

    DB query

--- App/Db.php
+++ App/Db.php
@@ -15,9 +15,29 @@ class Db
 
     public function __construct()
     {
-        echo ('Hello DB!');
+        //echo ('Hello DB!');
         $this->dbh = new \PDO('mysql:host=127.0.0.1;dbname=test', 'root', '');
+        //var_dump($this->dbh);
         
+    }
+
+    // выполняет переданный SQL-запрос и возвращает данные
+    public function query($sql)
+    {
+        $sth = $this->dbh->prepare($sql); // подготавливаем запрос
+        $res = $sth->execute();
+        //var_dump($sth);
+        echo "</br>";
+        var_dump($res);
+
+        $data = $sth->fetchAll();
+        echo "</br>";
+        var_dump($data);
+
+        if (false !== $res){
+            return $data;
+        }
+        return [];
     }
 
 }
