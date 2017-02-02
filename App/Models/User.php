bc1a35cb3548672b3f0033e16e9dd4813bc33ba4
KatyaVasina <ddjcnjrr0@mail.ru>
2017-02-02 17:59:23 +0300

    DB query

---
+++ App/Models/User.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vazhdaev
+ * Date: 02.02.2017
+ * Time: 14:06
+ */
+
+namespace App\Models;
+
+
+class User
+{
+    public $name;
+    public $email;
+}
