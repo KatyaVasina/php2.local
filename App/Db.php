a16681aeb5ec06dc815bde8bd9faffa52fa087d2
KatyaVasina <ddjcnjrr0@mail.ru>
2017-02-20 00:06:08 +0300

    for test001

--- App/Db.php
+++ App/Db.php
@@ -1,22 +1,46 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: master
- * Date: 28.01.2017
- * Time: 13:24
- */
 
 namespace App;
 
+echo ' - Db - ';
+
+//echo (file_exists("App/Singleton.php"))?"Существует":"Не существует";
+require "App/Singleton.php";
+
+
+//trait Singleton
+//{
+//    protected static $instance;
+//
+//    protected function __construct()
+//    {
+//    }
+//    public static function instance()
+//    {
+//        if (null === static::$instance) {
+//            static::$instance = new static;
+//            //echo '--Singleton instance--';
+//            //\App\showMessage::showM(' - Singleton instance - ');
+//        }
+//        return static::$instance;
+//    }
+//}
 
 class Db
+//extends Singleton
 {
+    use Singleton;
+
     protected $dbh;
 
-    public function __construct()
+    protected function __construct()
     {
-        //echo ('Hello DB!');
+        //echo " - db 001 - "; 
+        //\App\showMessage::showM(' - db construct - ');
+        //die();
+        echo (' - Db constr');
         $this->dbh = new \PDO('mysql:host=127.0.0.1;dbname=test', 'root', '');
+
         //var_dump($this->dbh);
         
     }
@@ -24,13 +48,14 @@ class Db
     // выполняет переданный SQL-запрос и возвращает данные
     public function query($sql, $class)
     {
-        echo "</br>";
-        echo $sql;
+        //\App\showMessage::showM(' - db query - ');
+        echo ' - db query - ';
+        //\App\showMessage::showM($sql);
+
         $sth = $this->dbh->prepare($sql); // подготавливаем запрос
         $res = $sth->execute();
         //var_dump($sth);
-        echo "</br>";
-        var_dump($res);
+//        var_dump($res);
 
         $data = $sth->fetchAll(\PDO::FETCH_CLASS, $class);
         echo "</br>";
